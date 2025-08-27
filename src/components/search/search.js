3fbfdd2dec1cf8c02f8ad35ee73489a736dd237d
Jay <jaynvnit@gmail.com>
2025-08-27 18:15:26 +0530

    rectified the error in loadOptions

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,35 +1,48 @@
-import {useState} from "react";
+import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import {geoApiOptions, url} from "../../api.js";
+import { geoApiOptions, url } from "../../api.js";
 
-const Search = ({onSearchChange}) => {
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
 
-    const [search, setSearch] = useState(null);
-
-    const loadOptions = (inputValue) => {
-        return fetch(
-            `{url}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
-            geoApiOptions
-        )
-        .then((response) => response.json())
-        .then((response) => console.log(response))
-        .catch((err) => console.error(err));
-    }
-
-    const handleOnChange = (searchData) => {
-        setSearch(searchData);
-        onSearchChange(searchData);
-    };
-
-    return (
-        <AsyncPaginate 
-            placeholder="Search for city"
-            debounceTimeout={600}
-            value={search}
-            onChange={handleOnChange}
-            loadOptions={loadOptions}
-        />
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${url}/cities?namePrefix=${inputValue}`,
+      geoApiOptions
     )
-}
+      .then((response) => response.json())
+      .then((response) => {
+      const optionsArray = Array.isArray(response.data)
+        ? response.data.map((city) => ({
+            value: `${city.latitude} ${city.longitude}`,
+            label: `${city.name}, ${city.countryCode}`,
+          }))
+        : [];
+      return {
+        options: optionsArray,
+        // Optionally add hasMore and additional here if paginating
+      };
+    })
+    .catch((err) => {
+      console.error(err);
+      return { options: [] }; // Always return an object with options key
+    });
+  };
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
 
 export default Search;
