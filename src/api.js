9117937fc452e9dd8afa55fb8644bba42a2a5be3
Jay <jaynvnit@gmail.com>
2025-08-27 17:40:10 +0530

    creation of api.js

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'bd4ed004ddmsh3daf7199c133fa6p14c97fjsna89cba25f4be',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
